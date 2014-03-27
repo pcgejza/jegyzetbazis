7e43d698b3ef5ae0f5ef0b661764b54d5a16629c
Erik <pcgejza@gmail.com>
2014-03-27 16:25:24 +0100

    teszt commit222

--- src/Frontend/LayoutBundle/Controller/DefaultController.php
+++ src/Frontend/LayoutBundle/Controller/DefaultController.php
@@ -1,11 +1,11 @@
 <?php
-
 namespace Frontend\LayoutBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    
     public function indexAction($name)
     {
         return $this->render('FrontendLayoutBundle:Default:index.html.twig', array('name' => $name));
