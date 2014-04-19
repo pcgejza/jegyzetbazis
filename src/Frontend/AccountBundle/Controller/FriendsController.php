289cb0e1ee22a12a5dcfa2559334a3eb86317f02
unknown <pcgejza@gmail.com>
2014-04-20 00:54:51 +0200

    barátnak jelölés fejlesztese , VAN MEG MIT RAJTA FEJLESZTENI

--- src/Frontend/AccountBundle/Controller/FriendsController.php
+++ src/Frontend/AccountBundle/Controller/FriendsController.php
@@ -4,6 +4,9 @@ namespace Frontend\AccountBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Frontend\AccountBundle\Entity\Friends;
+
 class FriendsController extends Controller{
     
     public function indexAction(){
@@ -37,10 +40,73 @@ class FriendsController extends Controller{
                         ->getFriendsStatus($MyUser, $viewedUser);
             
             return $this->render('FrontendAccountBundle:Friends:globalFriendsButtons.html.twig',array(
-                'FriendsObject' => $FriendsObject
+                'FriendsObject' => $FriendsObject,
+                'ViewedUser' => $viewedUser
             ));
         } catch (Exception $ex) {
             throw new \ErrorException('Hiba a getFriendsButtonsAction-ben : '+$ex->getMessage());
+        }
+    }
+    
+    public function friendsAddOrRemoveAction(){
+        try{
+            $MyUser = $this->get('security.context')->getToken()->getUser();
+            $viewedUserId = $this->get('request')->request->get('userID');
+            
+            if(!is_object($MyUser))
+                throw new Exception('Nincs bejelentkezve!');
+            
+            if(!is_numeric($viewedUserId))
+                throw new Exception('Nincs megadva a userID!');
+            
+            $em = $this->getDoctrine()->getEntityManager();
+            $viewedUser = $em->getReference('FrontendLayoutBundle:User', $viewedUserId);
+                
+            $FriendsObject = $this->getDoctrine()->getRepository('FrontendAccountBundle:Friends')
+                        ->getFriendsStatus($MyUser, $viewedUser);
+            
+            $text = "";
+            
+            if($FriendsObject == NULL){
+                $FriendsObject = new Friends();
+                $FriendsObject->setUserA($MyUser);
+                $FriendsObject->setUserB($viewedUser);
+                $FriendsObject->setMarkDate(new \DateTime('now'));
+                $text = 'Jelölés törlése';
+            }else{
+                if($FriendsObject->getUserA() == $MyUser){
+                    if($FriendsObject->getStatus() == 'selected'){
+                        $text = 'Barátnak jelölés';
+                        $FriendsObject->setStatus('selected-rejected');
+                    }elseif($FriendsObject->getStatus() == 'rejected' || $FriendsObject->getStatus() == 'selected-rejected'){
+                        $text = 'Jelölés törlése';
+                        $FriendsObject->setStatus('selected');
+                    }elseif($FriendsObject->getStatus() == 'active'){
+                        $text = 'Barátnak jelölés';
+                        $FriendsObject->setStatus('selected-rejected');
+                    }
+                }else{
+                    if($FriendsObject->getStatus() == 'selected'){
+                        $text = 'Barátnak jelölés';
+                        $FriendsObject->setStatus('selected-rejected');
+                    }elseif($FriendsObject->getStatus() == 'rejected' || $FriendsObject->getStatus() == 'selected-rejected'){
+                        $text = 'Jelölés törlése';
+                        $FriendsObject->setStatus('selected');
+                    }elseif($FriendsObject->getStatus() == 'active'){
+                        $text = 'Barátnak jelölés';
+                        $FriendsObject->setStatus('selected-rejected');
+                    }
+                }
+            }
+            $em->persist($FriendsObject);
+            $em->flush();
+            
+            
+            return new JsonResponse(array(
+                'text' => $text
+            ));
+        } catch (Exception $ex) {
+            return new JsonResponse(array('err'=>$ex->getMessage()));
         }
     }
 }
--- src/Frontend/AccountBundle/Resources/public/js/FriendsShared.js
+++ src/Frontend/AccountBundle/Resources/public/js/FriendsShared.js
@@ -1,22 +1,86 @@
 FriendsShared = {
     
-    ismerosnekJelolesGomb: null,
+    addOrRemoveURL: null,
     
     init: function(){
       
-        this.ismerosnekJelolesGomb = $('.friendMarking');
         this.bindUIActions();
     },
     
     bindUIActions: function(){
         this.bindMarkButton();
+        this.addQtipToFriendButtons();
     },
     
     bindMarkButton: function(){
-        this.ismerosnekJelolesGomb.unbind('click');
-        this.ismerosnekJelolesGomb.bind('click', function(){
-           alert('barátnak jelölés!'); 
+        $('.friendMarking').unbind('click');
+        $('.friendMarking').bind('click', function(){
+          var thisElement = $(this);
+          var userID = $(this).attr('userid');
+          var type = $(this).attr('type') ? $(this).attr('type') : null;
+          
+          var qtip = thisElement.parents('.qtip').first();
+          if(qtip.length>0){
+            qtip.qtip('destroy');
+            thisElement = $('.myFriend[userid="'+userID+'"]');
+            thisElement.removeClass('myFriend');
+            thisElement.removeClass('down-ico');
+            thisElement.addClass('friend-mark-button friendMarking');
+            FriendsShared.bindMarkButton();
+          }
+          
+          thisElement.addClass('loading');
+          $.post(FriendsShared.addOrRemoveURL, {
+              userID : userID,
+              type : type
+          }).done(function(data){
+             if(!data.err){
+                thisElement.html(data.text);
+                thisElement.removeClass('loading');
+                 
+                if(thisElement.hasClass('add')){
+                    thisElement.removeClass('add');
+                    thisElement.addClass('remove');
+                }else{
+                    thisElement.removeClass('remove');
+                    thisElement.addClass('add');
+                }
+             }else{
+                 alert('hiba : '+data.err);
+             }
+          });
         });
+        
+    },
+    
+    addQtipToFriendButtons: function(){
+       $('.myFriend').each(function(){
+          var uid = $(this).attr('userid');
+          var ht = $('.qtip-to-myfriend[userid="'+uid+'"]').html();
+           $(this).qtip({
+           content: {
+                text: ht,
+            },
+            show: 'click',
+            hide:  {
+              event: 'click'
+            },   
+            position: {
+                   my: 'top center',  // Position my top left...
+                   at: 'bottom center', // at the bottom right of...
+                   target:this.headerUserBox // my target
+               }, 
+            style: {
+                def: false,
+                classes: Shared.qtipStyleClass
+            },
+            events: {
+                render: function(){
+                    FriendsShared.bindMarkButton();
+                }
+            }
+        });
+       });
     },
     
 }
