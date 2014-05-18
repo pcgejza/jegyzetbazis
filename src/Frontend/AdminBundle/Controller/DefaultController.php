4c6fa1ea5c6f79e7ed079f691b761ddfcf1a73e0
unknown <pcgejza@gmail.com>
2014-05-18 17:56:34 +0200

    admin fejlesztes

--- src/Frontend/AdminBundle/Controller/DefaultController.php
+++ src/Frontend/AdminBundle/Controller/DefaultController.php
@@ -4,10 +4,65 @@ namespace Frontend\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function loginAction()
     {
-        return $this->render('FrontendAdminBundle:Default:index.html.twig', array('name' => $name));
+        $request = $this->get('request');
+        
+        $returnArray = array();
+        
+        
+        if ($this->get('security.context')->isGranted('ROLE_ADMIN') === true){
+            $url = $this->generateUrl('admin_list');
+            return $this->redirect($url);
+        }
+        
+        if($request->isMethod('POST')){
+            try{  
+                $email = $request->request->get('email');
+                $pass = $request->request->get('pass');
+                $userManager = $this->get('fos_user.user_manager');
+                $User = $userManager->findUserByUsernameOrEmail($email);
+
+                if($User === null)
+                   throw new Exception('Hibás email cím vagy rossz a beírt jelszó');
+
+                $encoder_service = $this->get('security.encoder_factory');
+                $encoder = $encoder_service->getEncoder($User);
+                $encoded_pass = $encoder->encodePassword($pass, $User->getSalt());
+
+                // jelszó ellenőrzés
+                if($User->getPassword() !== $encoded_pass){
+                    throw new Exception('Hibás email cím vagy rossz a beírt jelszó');
+                }
+                $token = new UsernamePasswordToken($User, $User->getPassword(), 'main', $User->getRoles());
+                $context = $this->get('security.context');
+                $context->setToken($token);
+            } catch (Exception $ex) {
+                $returnArray['err'] = $ex->getMessage();
+            }
+        }
+        
+        
+        if ($this->get('security.context')->isGranted('ROLE_ADMIN') === true){
+            $url = $this->generateUrl('admin_list');
+            return $this->redirect($url);
+        }
+        
+        if ($this->get('security.context')->isGranted('ROLE_USER') === true){
+            $returnArray['err'] = 'Nincs jogosultságod az adminhoz, mert te csak egy sima felhasználó vagy!';
+        }
+            
+        return $this->render('FrontendAdminBundle:Default:index.html.twig', $returnArray);
+    }
+    
+    public function listAction(){
+        
+        return $this->render('FrontendAdminBundle:Default:list.html.twig');
     }
 }
--- src/Frontend/AdminBundle/Menu/DefaultMenuBuilder.php
+++ src/Frontend/AdminBundle/Menu/DefaultMenuBuilder.php
@@ -11,45 +11,18 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 class DefaultMenuBuilder extends ContainerAware
 {
 
-   protected $factory;
-       
-    /**
-    * @param \Knp\Menu\FactoryInterface $factory
-    */
-   public function __construct(FactoryInterface $factory)
-   {
-       $this->factory = $factory;
-   }
-    
-    public function createAdminMenu(Request $request)
-    {   
-        $menu = $this->factory->createItem('root');
-        
-	
-        //$menu->addChild('Menü', array('uri' => '/admin_bundle/menu'));
-        $menu->addChild('Tantárgyak', array('uri' => '/admin/subjects'));
-        return $menu;
-    }
-	
-	public function createSidebarMenu(Request $request){
-		
-		$menu = $this->factory->createItem('sidebar');
-		$menu->addChild('Home', array('route' => 'homepage'));
-        return $menu;
-	}
-	
-    
-	
-    protected function addNavLinkURI($menu, $label, $uri)
+    public function adminMenu(FactoryInterface $factory, array $options)
     {
-        $item = $menu->addChild($label, array('uri' => $uri));
-        $item->setExtra('translation_domain', $menu->getExtra('translation_domain'));
-        $menu->setExtra('request_uri', $menu->getExtra('request_uri'));
+        $menu = $factory->createItem('root');
 
-        if ($item->getUri() == $menu->getExtra('request_uri')) {
-          $item->setAttribute('class', 'active');
-        }
+        $menu->addChild('Home', array('route' => 'frontend_index_homepage'));
+        $menu->addChild('About Me', array(
+            'route' => 'frontend_index_homepage',
+            'routeParameters' => array('id' => 42)
+        ));
+        // ... add more children
 
-        return $item;
+        return $menu;
     }
+    
 }
